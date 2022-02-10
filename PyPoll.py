9cbcf153b21dde7ddc5799b917374b1ca4af5530
Justin K <justinhkiang@gmail.com>
2022-02-09 16:12:37 -0800

    update voting analysis

--- PyPoll.py
+++ PyPoll.py
@@ -12,7 +12,9 @@ import os
 # file_to_load = 'resources/election_results.csv'
 
 #INDIRECT PATH
+#assign a variable to LOAD a file FROM a path
 file_to_load = os.path.join("Resources","election_results.csv")
+#assign a variable to SAVE the file TO a path
 file_to_save = os.path.join("analysis","voting_analysis.txt")
 
 
@@ -30,25 +32,64 @@ file_to_save = os.path.join("analysis","voting_analysis.txt")
 # outfile.close()
 
 with open(file_to_save,'w') as txt_file:
-    txt_file.write("Counties in the Election\n----------------------------\n")
+    txt_file.write("Counties in the Election\n---------------------------\n")
     txt_file.write("Arapahoe\nDenver\nJefferson")
+
+#initialize vote counter (has to be before we open the file because we want the count to be 0 each time we run the program)
+total_votes = 0
+
+#initialize candidate options list
+candidate_options = []
+
+#initialize dictionary to hold each candidate's votes
+candidate_votes = {}
+
+#declare variables for winner
+winner = ""
+winning_count = 0
+winning_percentage = 0
 
 #read the file object with reader function
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
-    # for row in file_reader:
-    #     print(row)
+    
+    #'next' method will "read" the header row
     headers = next(file_reader)
-    print(headers)
 
-print("hello")
+    #print each row in the CSV file
+    for row in file_reader:
+        total_votes += 1
 
-#total number of votes cast
+        #complete list of candidates who received votes
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        #tally votes for each candidate
+        candidate_votes[candidate_name] += 1
 
-#complete list of candidates who received votes
+    #total number of votes cast
+    print("Total vote count: ",total_votes)
 
-#percentage of votes per candidate
+    #list of candidates + total votes per candidate
+    print(candidate_votes)
 
-#total number of votes each candidate won
+#calc percentage of votes per candidate
+for candidate_name in candidate_votes:
+    votes = candidate_votes[candidate_name]
+    vote_percentage = float(votes / total_votes) * 100
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
-#winner of election based on popular vote
+    #determine winner based on popular vote
+    if candidate_votes[candidate_name] > winning_count and vote_percentage > winning_percentage:
+        winning_count = votes
+        winning_percentage = vote_percentage
+        winner = candidate_name
+    
+winner_summary = (
+    f"------------------------------\n"
+    f"Winner: {winner}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"------------------------------\n")
+print(winner_summary)
